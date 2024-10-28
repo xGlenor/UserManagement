4a593984ae371cd4fe0ed8dc04d7090345aeb4e8
Grzegorz Duraj <grzegorzduraj2001@gmail.com>
2024-10-28 12:12:46 +0100

    Change MSServer to SQLite

--- UserManagement/Program.cs
+++ UserManagement/Program.cs
@@ -16,7 +16,8 @@ var builder = WebApplication.CreateBuilder(args);
 |--------------------------------------------------------------------------
 */
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite("Data Source=UserManagement.db"));
+    /*options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));*/
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
